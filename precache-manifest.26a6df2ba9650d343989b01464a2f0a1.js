c003694efa8af7a3de4a80f66cb8638c0ea7c0dc
Anna Sudol <anjasudol@gmail.com>
2019-05-26 16:58:36 +0200

    Updates

---
+++ precache-manifest.26a6df2ba9650d343989b01464a2f0a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e749b1cdd70c5aab1cc2cbace07a7f",
+    "url": "/workshop-32/index.html"
+  },
+  {
+    "revision": "0956c9c804cb80632cfd",
+    "url": "/workshop-32/static/css/main.da389c1c.chunk.css"
+  },
+  {
+    "revision": "0dd7797e12a7e69e5db8",
+    "url": "/workshop-32/static/js/2.b3ce9216.chunk.js"
+  },
+  {
+    "revision": "d5b38f8829e65899eb8a",
+    "url": "/workshop-32/static/js/3.657fc021.chunk.js"
+  },
+  {
+    "revision": "e72b082cfc96199655f0",
+    "url": "/workshop-32/static/js/4.f6102dbc.chunk.js"
+  },
+  {
+    "revision": "0956c9c804cb80632cfd",
+    "url": "/workshop-32/static/js/main.347100b4.chunk.js"
+  },
+  {
+    "revision": "d62be35442e38d9c35bc",
+    "url": "/workshop-32/static/js/runtime~main.881ac284.js"
+  }
+]);
